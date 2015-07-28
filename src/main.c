80817f6dbc0c545c45223de6b3195d10378dc4ec
kristofwillen <kristof.willen@gmail.com>
2015-07-28 10:47:13 +0200

    Batterybar implementation

--- src/main.c
+++ src/main.c
@@ -5,8 +5,9 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_city_layer, *s_date_layer, *s_battery_layer;
 static InverterLayer *s_night_layer;
-static BitmapLayer *s_background_layer, *s_star_layer, *s_batterybar_layer;
+static BitmapLayer *s_background_layer, *s_star_layer;
 static Layer *s_line_layer;
+static int batteryLevel = 100;
 
 bool RandomFlag = true;
 char key_random[] = "1";
@@ -18,7 +19,7 @@ char citystr[] = "_SAN_FRANCISCO_";
 static int backgr_res;
 
 const int NBR_OF_CITIES = 31;
-char *citylist[NBR_OF_CITIES] = {"Athens", "Atlantis",
+char *citylist[31] = {"Athens", "Atlantis",
                       "Barcelona", "Berlin", "Boston", "Brussels", "Budapest", 
                       "Cairo", "Chicago", 
                       "Dallas", "Delhi", "Dublin", "Dubai",
@@ -34,22 +35,28 @@ char *citylist[NBR_OF_CITIES] = {"Athens", "Atlantis",
                       "Washington"};
 
 static void battery_handler(BatteryChargeState new_state) {
+  
   // Write to buffer and display
   static char s_battery_buffer[32];
   snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%% ", new_state.charge_percent);
   text_layer_set_text(s_battery_layer, s_battery_buffer);
-  bitmap_layer_destroy(s_batterybar_layer);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "[DBUG] Battery level=%i", (int)new_state.charge_percent);
-  s_batterybar_layer = bitmap_layer_create(GRect(22,62,(int)new_state.charge_percent,2));
-  
+  batteryLevel = (int)new_state.charge_percent;
 }
 
 
 void drawtimeline(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
+  
+  //graphics_context_set_stroke_color(ctx, GColorWhite);
+  //graphics_draw_line(ctx, GPoint(22,57), GPoint(122,57));
+  //graphics_draw_line(ctx, GPoint(22,58), GPoint(122,58));
+  
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_line(ctx, GPoint(0,0), GPoint(108,0));
   graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(0,1,batteryLevel,4), 0, GCornerNone);
+  
 }
+
 
 static int resource_background(char citystring[]) {
   if (strcmp(citystr, "Paris")              == 0) { return RESOURCE_ID_PARIS_BACKGROUND;     }
@@ -122,6 +129,7 @@ static void update_time() {
       layer_set_hidden((Layer *)s_star_layer, true); 
     }
   }
+  
   
   // Random, change every 2 hours
   if (RandomFlag) {
@@ -199,7 +207,7 @@ static void main_window_load(Window *window) {
   s_background_bitmap = gbitmap_create_with_resource(backgr_res);
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 5, 139, 55));
+  s_time_layer = text_layer_create(GRect(5, 0, 139, 55));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
@@ -228,7 +236,8 @@ static void main_window_load(Window *window) {
   text_layer_set_text(s_city_layer, citystr);
     
   //Time
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_POCKET_CALCULATOR_SIZE_54));
+  //s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_POCKET_CALCULATOR_SIZE_54));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DS_DIGITAL_54));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
@@ -251,13 +260,10 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_battery_layer));
 
   //Draw line under time
-  s_line_layer = layer_create(GRect(22,61,102,1));
-  layer_add_child(window_get_root_layer(window), s_line_layer);
+  s_line_layer = layer_create(GRect(20,59,108,4));
   layer_set_update_proc(s_line_layer,drawtimeline);
-  
-  // Battery bar
-  s_batterybar_layer = bitmap_layer_create(GRect(22,62,102,2));
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_batterybar_layer));
+  layer_add_child(window_get_root_layer(window), s_line_layer);
+
   
   // Nightlayer
   s_star_layer = bitmap_layer_create(GRect(0,0,144,120));
